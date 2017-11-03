e9df3cefc891607bf2ca6a14b6c623c7d4f12a03
“Yatin <yatin.nandwani@gmail.com>
2017-11-03 19:11:07 +0530

    adding UtilityClasses.py which contains CustomLRScheduler, main1.py - runs overall engine, config2.yml - sample config file

---
+++ UtilityClasses.py
@@ -0,0 +1,243 @@
+from __future__ import print_function
+import numpy as np
+#import ImageNetMiniDataLoader as inmd
+import torch
+from IPython.core.debugger import Tracer
+
+import torchvision.transforms as transforms
+from torch.utils.data import Dataset, DataLoader
+import os,sys,shutil
+from torch.optim import lr_scheduler
+#import POSDataLoader as posd
+
+
+def getVQATrainAndValidationLoader(config):
+    pass
+
+def getVQATestLoader(config):
+    pass
+
+"""
+def getTrainAndValidationLoaderCifar10(config):
+    transform = transforms.Compose(
+            [transforms.ToTensor(),
+            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+            #transforms.RandomHorizontalFlip()
+            ])
+
+    dataPath = os.path.join(os.getcwd(),config['data']['path'])
+    trainset = torchvision.datasets.CIFAR10(root=dataPath, train=True,
+                                        download=True, transform=transform)
+    
+    # load the dataset
+    #having it seperate because transormations could be different, for now they are same
+    validset = torchvision.datasets.CIFAR10(root=dataPath, train=True, 
+                download=True, transform=transform)
+    numTrain = len(trainset)
+    indices = list(range(numTrain))
+    split = int(np.floor(config['data']['validation_ratio']* numTrain))
+    # 
+    if config['data']['shuffle'] == True:
+        np.random.seed(config['data']['random_seed'])
+        np.random.shuffle(indices)
+    #
+    trainIdx, validIdx = indices[split:], indices[:split]
+
+    trainsampler = torch.utils.data.sampler.SubsetRandomSampler(trainIdx)
+    validsampler = torch.utils.data.sampler.SubsetRandomSampler(validIdx)
+
+    trainloader = torch.utils.data.DataLoader(trainset,sampler=trainsampler, **config['data']['loader_params'])
+    validloader = torch.utils.data.DataLoader(validset,sampler=validsampler,**config['data']['loader_params'])
+    return(trainloader,validloader)
+
+def getPOSDataLoaderFromFile(config):
+    dataPath = os.path.join(os.getcwd(),config['data']['path'])
+    sentencePath = config['data']['sentence_path']
+    tagPath = config['data']['tag_path']
+    testSet = posd.POSDataSet(dataPath,train=False,validation=False,sentenceFileName=sentencePath,tagFileName=tagPath)
+    loader = torch.utils.data.DataLoader(testSet,**config['data']['loader_params'])
+    return(loader)
+
+def getPOSDataLoader(config):
+    dataPath = os.path.join(os.getcwd(),config['data']['path'])
+    trainSet = posd.POSDataSet(dataPath,train=True,validation=False)
+    validSet = posd.POSDataSet(dataPath,train=False,validation=True)
+    numTrain = len(trainSet)
+    #indices = list(range(numTrain))
+    # 
+    #if config['data']['shuffle'] == True:
+    #    np.random.seed(config['data']['random_seed'])
+    #    np.random.shuffle(indices)
+    #
+    #trainsampler = torch.utils.data.sampler.SubsetRandomSampler(indices)
+    #trainloader = torch.utils.data.DataLoader(trainSet,sampler=trainsampler, **config['data']['loader_params'])
+    trainloader = torch.utils.data.DataLoader(trainSet, **config['data']['loader_params'])
+    validloader = torch.utils.data.DataLoader(validSet,**config['data']['loader_params'])
+    return(trainloader,validloader)
+"""
+
+def customSentenceBatcher(loader,batchSize=1):
+    batchNumber = -1
+    runningInputs = []
+    runningLabels = []
+    runningSize = -1
+    for i,(si,sl) in enumerate(loader,0):
+        ts = si.size()[1]
+        if (len(runningInputs) != 0) and ((ts != runningSize) or len(runningInputs) == batchSize):
+            inputs = torch.cat(runningInputs,0)
+            labels = torch.cat(runningLabels,0)
+            batchNumber += 1
+            yield(batchNumber,(inputs,labels))
+            runningInputs = []
+            runningLabels = []
+        #
+        runningSize = ts
+        runningInputs.append(si)
+        runningLabels.append(sl)
+
+        
+"""         
+def getTrainAndValidationLoaderImagenetMini(config):
+    transform = transforms.Compose(
+            [transforms.Scale((256,256)), 
+            transforms.RandomCrop(227),
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.485, 0.456, 0.406],
+            std=[0.229, 0.224, 0.225])
+            ])
+    validationTransforms = transforms.Compose([
+            transforms.Scale((256,256)),
+            transforms.CenterCrop(227),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.485, 0.456, 0.406],
+            std=[0.229, 0.224, 0.225])
+            ]) 
+
+    dataPath = os.path.join(os.getcwd(),config['data']['path'])
+    trainset = inmd.ImagenetMiniDataSet(rootDir=dataPath, train=True,
+                                      transform=transform)
+    # 
+    validset = inmd.ImagenetMiniDataSet(rootDir=dataPath, validation=True, 
+                 transform=validationTransforms)
+    numTrain = len(trainset)
+    indices = list(range(numTrain))
+    # 
+    if config['data']['shuffle'] == True:
+        np.random.seed(config['data']['random_seed'])
+        np.random.shuffle(indices)
+    #
+    trainsampler = torch.utils.data.sampler.SubsetRandomSampler(indices)
+    trainloader = torch.utils.data.DataLoader(trainset,sampler=trainsampler, **config['data']['loader_params'])
+    validloader = torch.utils.data.DataLoader(validset,**config['data']['loader_params'])
+    return(trainloader,validloader)
+
+def getTestLoaderImagenetMini(config):
+    transform = transforms.Compose([
+            transforms.Scale((256,256)),
+            #transforms.CenterCrop(227),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.485, 0.456, 0.406],
+            std=[0.229, 0.224, 0.225])
+            ]) 
+
+    dataPath = os.path.join(os.getcwd(),config['data']['path'])
+    deepDir = True
+    if config['data'].has_key('deep_dir'):
+        deepDir = config['data']['deep_dir']
+    #
+    testset = inmd.ImagenetMiniDataSet(rootDir = dataPath,test = True,transform=transform,deepDir=deepDir)
+    testloader = torch.utils.data.DataLoader(testset,**config['data']['loader_params'])
+    return testloader
+
+"""
+def augmentTestBatchImagenetmini(inputs):
+    i5 = inputs[:,:,14:14+227,14:14+227] 
+    i1 = inputs[:,:,0:227,0:227]
+    i2 = inputs[:,:,29:256,29:256]
+    i3 = inputs[:,:,0:227,29:256]
+    i4 = inputs[:,:,29:256,0:227]
+    return [i5,i1,i2,i3,i4]
+
+
+
+def getTestLoaderCifar10(config):
+    transform = transforms.Compose(
+            [transforms.ToTensor(),
+            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+            #transforms.RandomHorizontalFlip()
+            ])
+
+    dataPath = os.path.join(os.getcwd(),config['data']['path'])
+    testset = torchvision.datasets.CIFAR10(root=dataPath, train=False,
+                                       download=True, transform=transform)
+    testloader = torch.utils.data.DataLoader(testset,**config['data']['loader_params'])
+    return testloader
+
+
+class EWMA():
+    def __init__(self,memory):
+        self.memory = memory
+        self.reset()
+
+    def reset(self):
+        self.count = 0
+        self.normalizingFactor = 1
+        self.memoryToPowerCount = 1
+        self.sum = 0
+        self.x = 0
+        self.avg = 0
+        self.y1 = 0
+
+
+    def update(self,x,n = 1):
+        self.count += n
+        self.x = x
+        memoryToPowerN = self.memory**n
+        if (self.memory != 1):
+            factor =  (1 - memoryToPowerN)/(1 - self.memory)
+            self.memoryToPowerCount = self.memoryToPowerCount*memoryToPowerN
+            self.normalizingFactor = (1 - self.memoryToPowerCount)/(1 - self.memory) 
+        else:
+            self.normalizingFactor = self.count
+            factor = n
+        
+        #
+        self.sum = (memoryToPowerN)*self.sum + self.x*factor
+        self.avg = self.sum/self.normalizingFactor
+
+
+class CustomReduceLROnPlateau(lr_scheduler.ReduceLROnPlateau):
+    def __init__(self,optimizer,maxPatienceToStopTraining=20, kwargs={}):
+        super(CustomReduceLROnPlateau, self).__init__(optimizer,**kwargs)
+        self.unconstrainedBadEpochs = self.num_bad_epochs
+        self.maxPatienceToStopTraining = maxPatienceToStopTraining
+        self._init_getThresholdFn(self.mode,self.threshold,self.threshold_mode)
+
+    def _init_getThresholdFn(self, mode, threshold, threshold_mode):
+        if mode == 'min' and threshold_mode == 'rel':
+            rel_epsilon = 1. - threshold
+            self.getThresholdFn = lambda best: (best * rel_epsilon)
+        elif mode == 'min' and threshold_mode == 'abs':
+            self.getThresholdFn = lambda best: best - threshold
+        elif mode == 'max' and threshold_mode == 'rel':
+            rel_epsilon = threshold + 1.
+            self.getThresholdFn = lambda best: best * rel_epsilon
+        else:  # mode == 'max' and epsilon_mode == 'abs':
+            self.getThresholdFn = lambda best: best + threshold
+
+
+
+    def step(self, metrics, epoch=None):
+        if self.is_better(metrics, self.best):
+            self.unconstrainedBadEpochs = 0
+        else:
+            self.unconstrainedBadEpochs += 1
+        #
+        super(CustomReduceLROnPlateau,self).step(metrics,epoch)
+        
+
+    def shouldStopTraining(self):
+        self.currentThreshold = self.getThresholdFn(self.best)
+        print("Num_bad_epochs: {0}, unconstrainedBadEpochs: {1}, bestMetric: {2}, currentThreshold: {3}".format(self.num_bad_epochs, self.unconstrainedBadEpochs, self.best,self.currentThreshold)) 
+        return(self.unconstrainedBadEpochs > self.maxPatienceToStopTraining)
---
+++ main1.py
@@ -0,0 +1,359 @@
+
+from __future__ import print_function
+from datetime import datetime as dt
+
+import yaml,torch,shutil
+from torch.autograd import Variable
+
+import torch.nn.functional as F
+import torch.optim as optim
+import torch.nn as nn
+
+
+import os,sys
+import UtilityClasses as uc
+
+import time
+import argparse
+parser = argparse.ArgumentParser()
+from IPython.core.debugger import Pdb
+
+import vqa
+import san
+
+parser.add_argument('-c','--config',type=str,default='config.yml')
+
+def prefixId(config):
+    prefix = config['prefix']
+    if not config.has_key('model_name'):
+        config['model_name'] = prefix
+    #
+    save_dir = os.path.join(os.getcwd(),config['save_dir'])
+    save_dir = os.path.join(save_dir,prefix)
+
+    os.makedirs(save_dir, exist_ok=True)
+    for mk in ['checkpoints','stats']:
+        if config.has_key(mk):
+            for k in config[mk].keys():
+                f = config[mk][k]
+                config[mk][k] = os.path.join(save_dir,f)
+    #
+    return config
+
+def printHeaders(config):
+    #print("time,epoch,nbatches,nsentences,nwords,top1,top5,loss,trainingTime")
+    if(config['stats'].has_key('trainTimeLogs')):
+        fh = open(config['stats']['trainTimeLogs'],'a+')
+        print("time,epoch,nbatches,nsentences,nwords,top1,top5,loss,trainingTime,dataLoadingTime,lr,whichModel",file =fh)
+        fh.close()
+    #
+    fh = open(config['stats']['accuracyLogs'],'a+')
+    print("time,epoch,nbatches,nsentences,nwords,top1,top5,loss,dataLoadingTime,trainingTime,lr,whichSet,whichModel",file=fh)
+    print("time,epoch,nbatches,nsentences,nwords,top1,top5,loss,dataLoadingTime,trainingTime,lr,whichSet,whichModel")
+
+def accuracy(tag_scores,labels,topk = (1,),dataloader=None,writeToFile=False,fh=None):
+    maxk = max(topk)
+    batchSize = labels.size()[0]
+    nwords = labels.size()[1]
+    _, pred = tag_scores.topk(maxk,2)
+    pred = pred.view(-1,maxk).t()
+    correct = pred.eq(labels.view(1,-1).expand_as(pred))
+    res = []
+    for k in topk:
+        correct_k = correct[:k].view(-1).float().sum(0, keepdim=True)
+        res.append(correct_k.mul_(1.0 / (batchSize*nwords)))
+    #
+    if dataloader is not None and batchSize == 1:
+        print(' '.join([dataloader.dataset.ix_to_tag[x] for x in pred[0,:]]),file=fh)
+    #
+    return res
+
+
+def validate(config,dataloader,net,criterion,optimizer,epoch,whichSet,whichModel):
+    batchTime = uc.EWMA(1)
+    losses = uc.EWMA(1)
+    top1 = uc.EWMA(1)
+    top5 = uc.EWMA(1)
+    dataLoadingTime = uc.EWMA(1)
+    net.eval()
+    asyncVar = config['data']['loader_params'].has_key('pin_memory') and config['data']['loader_params']['pin_memory']
+    end = time.time()
+    nsentences= 0
+    writeToFile = False
+    if whichSet == 'validationSet':
+        fileName= config['stats']['outputOverValidation']
+        writeToFile = config['training']['write_output_to_file']
+        ofh= open(fileName,'w+')
+
+    customBatchSize = 1
+    if(whichSet.lower() == 'trainset'):
+        customBatchSize = config['data']['custom_batch_size']
+
+    print('batch size: {0}'.format(customBatchSize))
+    for i,(inputs,labels) in uc.customSentenceBatcher(dataloader,customBatchSize):
+    #for i, (inputs, labels) in enumerate(dataloader, 0):
+        dataLoadingTime.update(time.time() - end) 
+         
+        if torch.cuda.is_available():
+            inputs,labels = inputs.cuda(), labels.cuda(async=asyncVar)
+
+        #
+        batchSize = labels.size()[0]
+        nwords = labels.size()[1]
+        nsentences  += batchSize
+        labels = torch.autograd.Variable(labels, volatile=True)
+        #need to extract 5 datasets from current batch and take average for prediction
+        inputs = torch.autograd.Variable(inputs, volatile=True)
+        # compute output
+        output = net(inputs)
+        #
+        loss = criterion(output.view(-1,config['data']['tagset_size']), labels.view(-1))
+        if writeToFile:
+            prec1, prec5 = accuracy(output.data, labels.data, topk=(1, 5),dataloader=dataloader,writeToFile=writeToFile,fh = ofh)
+        else:
+            prec1, prec5 = accuracy(output.data, labels.data, topk=(1, 5))
+
+        losses.update(loss.data[0],batchSize*nwords )
+        top1.update(prec1[0], batchSize*nwords)
+        top5.update(prec5[0], batchSize*nwords)
+        # measure elapsed time
+        batchTime.update(time.time() - end)
+        end = time.time()
+    #
+    lr = getLearningRate(optimizer)
+    fh = open(config['stats']['accuracyLogs'],'a+')
+    dtstr = str(dt.now())
+    #print("time,epoch,nbatches,nsentences,nwords,top1,top5,loss,trainingTime")
+    print("{0},{1},{2},{3},{4},{5:.3f},{6:.3f},{7:.3f},{8:.2f},{9:.2f},{10:.5f},{11},{12}".format(dtstr,epoch, batchTime.count,nsentences,top1.count, top1.avg, top5.avg, losses.avg, dataLoadingTime.sum,batchTime.sum,lr,whichSet,whichModel ),file=fh)
+    print("{0},{1},{2},{3},{4},{5:.3f},{6:.3f},{7:.3f},{8:.2f},{9:.2f},{10:.5f},{11},{12}".format(dtstr,epoch, batchTime.count,nsentences,top1.count, top1.avg, top5.avg, losses.avg, dataLoadingTime.sum,batchTime.sum,lr,whichSet,whichModel ))
+    fh.close()
+    return (losses.avg,top1.avg)
+
+
+def getLearningRate(optimizer):
+    for param_group in optimizer.param_groups:
+        return param_group['lr']
+
+def train(config,trainloader,net,criterion,optimizer,epoch):
+    net.train()
+    #Pdb().set_trace()
+    asyncVar = config['data']['loader_params'].has_key('pin_memory') and config['data']['loader_params']['pin_memory']
+    dataLoadingTime = uc.EWMA(1)
+    trainingTime = uc.EWMA(1)
+    losses = uc.EWMA(1)
+    top1 = uc.EWMA(1)
+    top5 = uc.EWMA(1)
+    end = time.time()
+    #Tracer()()
+    nsentences= 0
+    #for i, (inputs, labels) in enumerate(trainloader, 0):
+    for i,(inputs,labels) in uc.customSentenceBatcher(trainloader,config['data']['custom_batch_size']):
+        # get the inputs
+        dataLoadingTime.update(time.time() - end) 
+        batchSize = labels.size()[0]
+        nwords = labels.size()[1]
+        nsentences  += batchSize
+        #print("batchNumber: {0}, inputSize: {1}, labelSize:{2}, nsentences: {3}".format(i,str(inputs.size()), str(labels.size()), nsentences))
+        # wrap them in Variable
+        inputs, labels = Variable(inputs), Variable(labels)
+        if torch.cuda.is_available():
+            inputs,labels = inputs.cuda(), labels.cuda(async=asyncVar)
+        # zero the parameter gradients
+
+        # forward + backward + optimize
+        outputs = net(inputs)
+        loss = criterion(outputs.view(-1,config['data']['tagset_size']), labels.view(-1))
+        # measure accuracy and record loss
+        evaluationStartTime = time.time()
+        prec1, prec5 = accuracy(outputs.data, labels.data, topk=(1, 5))
+        losses.update(loss.data[0], batchSize*nwords)
+        top1.update(prec1[0], batchSize*nwords)
+        top5.update(prec5[0], batchSize*nwords)
+        
+        #compute gradient and do SGD step
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        trainingTime.update(time.time() - end)
+        end = time.time()
+        dtstr = str(dt.now())
+        if config.has_key('verbose') and config['verbose'] and i%100 == 0:
+            print("{0},{1},{2},{3},{4},{5:.3f},{6:.3f},{7:.3f},{8},{9}".format(dtstr,epoch, trainingTime.count,nsentences,top1.count, top1.avg, top5.avg, losses.avg, trainingTime.sum,config['model_name'] ))
+        
+        #if(i == 10):
+        #    break
+    #
+    #write to file and flush
+    fh = open(config['stats']['trainTimeLogs'],'a+')
+
+    lr = getLearningRate(optimizer)
+    dtstr = str(dt.now())
+
+    print("{0},{1},{2},{3},{4},{5:.3f},{6:.3f},{7:.3f},{8:.2f},{9:.2f},{10:.5f},{11}".format(dtstr,epoch, trainingTime.count,nsentences,top1.count, top1.avg, top5.avg, losses.avg, trainingTime.sum,dataLoadingTime.sum,lr,config['model_name'] ))
+    fh = open(config['stats']['trainTimeLogs'],'a+')
+    print("{0},{1},{2},{3},{4},{5:.3f},{6:.3f},{7:.3f},{8:.2f},{9:.2f},{10:.5f},{11}".format(dtstr,epoch, trainingTime.count,nsentences,top1.count, top1.avg, top5.avg, losses.avg, trainingTime.sum,dataLoadingTime.sum,lr,config['model_name'] ),file=fh)
+    fh.close()
+
+
+
+def saveCheckpoint(state, isBest, config):
+    torch.save(state, config['checkpoints']['path'])
+    if isBest:
+        print("isBest True. Epoch: {0}, bestError: {1}".format(state['epoch'],state['bestError']))
+        shutil.copyfile(config['checkpoints']['path'],config['checkpoints']['best_model_path'])
+
+
+def getLearningRate(optimizer):
+    for param_group in optimizer.param_groups:
+        return param_group['lr']
+
+
+
+
+def jobManager():
+    #3 modes - 
+        # train 1 model whose configuration is specified in the given config file 
+        # test 1 model whose configuration is specified in the given config file
+        # test bulk models - test all models specified in the given config file. All models are specified with their respective config files and an optional path to best model
+    global args
+    args = parser.parse_args()
+    args.config = os.path.join(os.getcwd(),args.config)
+    config = yaml.load(open(args.config))
+    if not config.has_key('mode'):
+        config['mode'] = 'train'
+
+    #Pdb().set_trace()
+    config =  prefixId(config)
+    printHeaders(config)
+    if config['mode'].lower() == 'train':
+        main(config,'train')
+    elif config['mode'].lower() == 'test':
+        main(config,'test',onlyTest=True)
+    elif config['mode'].lower() == 'bulk_test':
+        bulkTest(config)
+    else:
+        print("Incorrect mode. Expected train, test or bulk_test")
+        
+    print("Job Manager Finished")
+ 
+def bulkTest(config):
+    outputFilePath = config['stats']['accuracyLogs']
+    for model in config['testing']['models']:
+        thisConfigPath = model['config_path']
+        thisModelPath = model['path']
+        thisModelName = model['model_name']
+        
+        thisConfig  = yaml.load(open(thisConfigPath))
+        thisConfig['stats']['accuracyLogs'] = outputFilePath
+        thisConfig['checkpoints']['best_model_path'] = thisModelPath
+        #if(not thisConfig.has_key('model_name')):
+        thisConfig['data']['augment_test'] = config['data']['augment_test']
+        thisConfig['model_name'] = thisModelName
+        main(thisConfig,'test')
+
+
+def main(config,mode,onlyTest = False):
+    config['use_gpu'] = config['use_gpu'] and torch.cuda.is_available()
+    use_gpu = config['use_gpu']
+
+    if not onlyTest:
+        (trainloader,validationloader) = uc.getVQATrainAndValidationLoader(config)
+        testloader = uc.getVQATestLoader(config)
+        vocab_size = len(trainloader.dataset.word_to_ix)
+        output_size = len(validationloader.dataset.ans_to_ix)
+        
+    else:
+        testloader = uc.getPOSDataLoaderFromFile(config)
+        vocab_size = len(testloader.dataset.word_to_ix)
+        output_size = len(testloader.dataset.ans_to_ix)
+
+    config['model']['params']['vocab_size'] = vocab_size
+    config['model']['params']['output_size'] = output_size
+
+    if config['model']['which_model'] == 'vqa':
+        net = vqa.VQAModel(**config['model']['params'])
+    elif config['model']['which_model'] == 'san':
+        net = san.SANModel(**config['model']['params'])
+    #
+    criterion = nn.NLLLoss()
+
+    if(config['optim']['class'].lower() == 'sgd'):
+        optimizer = optim.SGD(net.parameters(),**config['optim']['params'])
+    else:
+        optimizer = optim.Adam(net.parameters(),**config['optim']['params'])
+    
+    if(use_gpu):
+        print("Cuda is True.....converting model and loss fn to .cuda")
+        net = net.cuda()
+        criterion = criterion.cuda()
+
+    startEpoch = 0
+    bestPrec1 = 0
+    bestError = 1
+    
+    pathForTrainedModel = config['checkpoints']['path']
+    
+    if (mode == 'test'):
+        pathForTrainedModel = config['checkpoints']['best_model_path']
+    
+    if ((config['training']['start_from_checkpoint']) or (mode == 'test')):
+        if os.path.exists(pathForTrainedModel):
+            print("=> loading checkpoint/model found at '{0}'".format(pathForTrainedModel))
+            checkpoint = torch.load(pathForTrainedModel)
+            startEpoch = checkpoint['epoch']
+            bestError = checkpoint['bestError']
+            net.load_state_dict(checkpoint['stateDict'])
+            optimizer.load_state_dict(checkpoint['optimizer'])
+        else:
+            print("=> no checkpoint found at '{0}'".format(pathForTrainedModel))
+            if mode == 'test':
+                print(" = > Error: could not find any model at the given path. Quitting...")
+                return
+    
+    if mode == 'test':
+        epoch = startEpoch - 1
+        testLoss,testPrec1 = validate(config,testloader, net, criterion, optimizer,epoch,'testSet',config['model_name'])  
+        if onlyTest:
+            return
+        validLoss,validPrec1 = validate(config,validationloader, net, criterion,optimizer,epoch,'validationSet',config['model_name'])
+        trainLoss,trainPrec1 = validate(config,trainloader, net, criterion,optimizer,epoch,'trainSet',config['model_name'])
+        return
+
+    #
+    #Tracer()
+    #Pdb().set_trace()
+    scheduler = uc.CustomReduceLROnPlateau(optimizer,config['optim']['scheduler_params']['maxPatienceToStopTraining'], config['optim']['scheduler_params']['base_class_params'])
+
+    print("Start from epoch#:",startEpoch)
+    
+    
+    for epoch in range(startEpoch, config['training']['no_of_epochs']):
+        #adjustLearningRate(config,optimizer,epoch)
+        train(config, trainloader, net, criterion, optimizer, epoch)
+         #evaluate on train set and validation set
+        validLoss1,validPrec1 = validate(config,validationloader, net, criterion,optimizer,epoch,'validationSet',config['model_name'])
+        trainLoss1,trainPrec1 = validate(config,trainloader, net, criterion,optimizer,epoch,'trainSet',config['model_name'])
+        # remember best prec@1 and save checkpoint
+        validError = 1.0 - validPrec1
+        isBest = validError < bestError
+        bestError = min(validError, bestError)
+        print("Input to scheduler step: {0}".format(validError))
+        scheduler.step(validError,epoch=epoch)
+        saveCheckpoint({
+            'epoch': epoch + 1,
+            'stateDict': net.state_dict(),
+            'bestError': bestError,
+            'optimizer' : optimizer.state_dict()
+            #'scheduler': scheduler.state_dict()
+        }, isBest,config)
+
+        if(scheduler.shouldStopTraining()):
+            print("Stop training as no improvement in accuracy - no of unconstrainedBadEopchs: {0} > {1}".format(scheduler.unconstrainedBadEpochs,scheduler.maxPatienceToStopTraining))
+            #Pdb().set_trace()
+            break
+    #
+    print('Finished Training')
+
+if __name__ == '__main__':
+    #Pdb().set_trace()
+    jobManager()
